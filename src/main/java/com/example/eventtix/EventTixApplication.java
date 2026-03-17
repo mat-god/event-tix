00b2d62b42b0f62a8f14d5189cec66309fcdd42d
mat-god <matgodlewski.py@gmail.com>
2026-03-17 17:11:07 +0100

    fix: gradle build

--- src/main/java/com/example/eventtix/EventTixApplication.java
+++ src/main/java/com/example/eventtix/EventTixApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventTixApplication {
 
-  void main() {
+  public static void main() {
     SpringApplication.run(EventTixApplication.class);
   }
 
